074d6bb6eaf238396d06837c8323b95b67c66969
Adam Dangoor <adamdangoor@gmail.com>
2020-01-06 01:16:59 +0000

    Progress towards using modern f-strings

--- tests/test_storage/test_storage.py
+++ tests/test_storage/test_storage.py
@@ -99,13 +99,10 @@ class TestCreateUser:
         )
         assert response.headers['Content-Type'] == 'application/json'
         assert response.status_code == codes.CONFLICT
+        email = USER_DATA['email']
         expected = {
-            'title':
-            'There is already a user with the given email address.',
-            'detail':
-            'A user already exists with the email "{email}"'.format(
-                email=USER_DATA['email'],
-            ),
+            'title': 'There is already a user with the given email address.',
+            'detail': f'A user already exists with the email "{email}"',
         }
         assert response.json == expected
 
@@ -145,19 +142,16 @@ class TestGetUser:
         A ``GET`` request for a user which does not exist returns a NOT_FOUND
         status code and error details.
         """
+        email = USER_DATA['email']
         response = storage_app.get(
-            '/users/{email}'.format(email=USER_DATA['email']),
+            f'/users/{email}',
             content_type='application/json',
         )
         assert response.headers['Content-Type'] == 'application/json'
         assert response.status_code == codes.NOT_FOUND
         expected = {
-            'title':
-            'The requested user does not exist.',
-            'detail':
-            'No user exists with the email "{email}"'.format(
-                email=USER_DATA['email'],
-            ),
+            'title': 'The requested user does not exist.',
+            'detail': f'No user exists with the email "{email}"',
         }
         assert response.json == expected
 
@@ -166,11 +160,8 @@ class TestGetUser:
         If a Content-Type header other than 'application/json' is given, an
         UNSUPPORTED_MEDIA_TYPE status code is given.
         """
-        response = storage_app.get(
-            '/users/{email}'.format(email=USER_DATA['email']),
-            content_type='text/html',
-        )
-
+        email = USER_DATA['email']
+        response = storage_app.get(f'/users/{email}', content_type='text/html')
         assert response.status_code == codes.UNSUPPORTED_MEDIA_TYPE
 
 
@@ -352,7 +343,7 @@ class TestGetTodo:
         item_id = create.json['todo_id']
 
         read = storage_app.get(
-            '/todos/{todo_id}'.format(todo_id=item_id),
+            f'/todos/{item_id}',
             content_type='application/json',
         )
 
@@ -377,7 +368,7 @@ class TestGetTodo:
         item_id = create.json['todo_id']
 
         read = storage_app.get(
-            '/todos/{todo_id}'.format(todo_id=item_id),
+            f'/todos/{item_id}',
             content_type='application/json',
         )
 
@@ -432,14 +423,14 @@ class TestDeleteTodo:
         item_id = create.json['todo_id']
 
         delete = storage_app.delete(
-            '/todos/{todo_id}'.format(todo_id=item_id),
+            f'/todos/{item_id}',
             content_type='application/json',
         )
 
         assert delete.status_code == codes.OK
 
         read = storage_app.get(
-            '/todos/{todo_id}'.format(todo_id=item_id),
+            f'/todos/{item_id}',
             content_type='application/json',
         )
 
@@ -458,12 +449,12 @@ class TestDeleteTodo:
         item_id = create.json['todo_id']
 
         storage_app.delete(
-            '/todos/{todo_id}'.format(todo_id=item_id),
+            f'/todos/{item_id}',
             content_type='application/json',
         )
 
         delete = storage_app.delete(
-            '/todos/{todo_id}'.format(todo_id=item_id),
+            f'/todos/{item_id}',
             content_type='application/json',
         )
 
